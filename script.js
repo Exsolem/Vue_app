1ef56f156950f71f7d8dad1e9d57fa98d921d067
Zabarny Alexander <extenebris.x@gmail.com>
2019-07-26 09:48:03 +0300

    Empty local storage check added

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-//localStorage.clear();
+const users = [
+    { name: 'Ivan', surname: 'Ivanov', phone: '+380349817492', email: 'Ivanov@mail.com' },
+    { name: 'Petr', surname: 'Petrov', phone: '+380349543492', email: 'Petrov@mail.com' },
+    { name: 'Sidor', surname: 'Sidorov', phone: '+380312817492', email: 'Sidorov@mail.com' },
+    { name: 'Semen', surname: 'Semenov', phone: '+380349817443', email: 'Semenov@mail.com' },
+    { name: 'Konstantin', surname: 'Konstantinov', phone: '+380349817491', email: 'Konstantinov@mail.com' },
+    { name: 'Pavel', surname: 'Pavlov', phone: '+380349817412', email: 'Pavlov@mail.com' },
+    { name: 'Evgeniy', surname: 'Jenkov', phone: '+380349817465', email: 'Jenkov@mail.com' },
+    { name: 'Alexander', surname: 'Kartoshkin', phone: '+380349817445', email: 'Kartoshkin@mail.com' }
+];
+    
+
 let app = new Vue({
     el: '#app',
     data: {
-        users: JSON.parse(localStorage.getItem('users')) || [
-            { name: 'Ivan', surname: 'Ivanov', phone: '+380349817492', email: 'Ivanov@mail.com' },
-            { name: 'Petr', surname: 'Petrov', phone: '+380349543492', email: 'Petrov@mail.com' },
-            { name: 'Sidor', surname: 'Sidorov', phone: '+380312817492', email: 'Sidorov@mail.com' },
-            { name: 'Semen', surname: 'Semenov', phone: '+380349817443', email: 'Semenov@mail.com' },
-            { name: 'Konstantin', surname: 'Konstantinov', phone: '+380349817491', email: 'Konstantinov@mail.com' },
-            { name: 'Pavel', surname: 'Pavlov', phone: '+380349817412', email: 'Pavlov@mail.com' },
-            { name: 'Evgeniy', surname: 'Jenkov', phone: '+380349817465', email: 'Jenkov@mail.com' },
-            { name: 'Alexander', surname: 'Kartoshkin', phone: '+380349817445', email: 'Kartoshkin@mail.com' }
-        ],
+        users: (JSON.parse(localStorage.getItem('users')) != '' ? JSON.parse(localStorage.getItem('users')) : users),
         newUser: { name: '', surname: '', phone: '', email: '' },
         pageFlag: true,
         JSONinput: ''
